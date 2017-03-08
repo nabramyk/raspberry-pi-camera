dd82b9fe94c00ebcdcc2a79a24e3febf2cc80061
Nathan Abramyk <nabramyk@gmail.com>
2017-03-08 13:46:26 -0600

    Stuff

--- raspi_cam_python_server.py
+++ raspi_cam_python_server.py
@@ -112,7 +112,7 @@ class myHandler(BaseHTTPRequestHandler):
 					'platform_machine':platform.machine(),
 					'platform_version':platform.version(),
 					'platform_system':platform.system(),
-					#'platform_processor':platform.processor(),
+					'platform_processor':'',
 					'storage_total':psutil.disk_usage('/').total,
 					'storage_used':psutil.disk_usage('/').used,
 					'storage_free':psutil.disk_usage('/').free,
