faf29db6067a912bd7a5cf3c34ec162f6663fdbf
Nathan Abramyk <nabramyk@gmail.com>
2017-04-04 16:13:12 -0600

    Removed unnecessary and irrelevant code and beginning to implement exclusive functionality for the local server.

--- raspi_cam_python_forward.py
+++ raspi_cam_python_forward.py
@@ -9,58 +9,16 @@ CAMERA_IP = "192.168.1.13"
 
 timelapse_running = Value(ctypes.c_bool, False)
 
-def interval_of_image_pulls(timelapse_running):
-	
-	while timelapse_running.value:
-		path = os.getcwd() + "/images/" + time.strftime('%Y.%m.%d') + "/"
-		if not os.path.exists(path):
-			os.makedirs(path)
-		conn = httplib.HTTPConnection(CAMERA_IP, CAMERA_PORT)
-		conn.request("GET", "/timelapse/download/")
-		image_data = conn.getresponse().read()
-		if not len(image_data)==0:
-			f = open(path + time.strftime('%H:%M:%S') + '.jpg', 'a+')
-			f.write(image_data)
-			f.close()
-		time.sleep(1)
-
 class myHandler(BaseHTTPRequestHandler):
 	
 	def do_GET(self):
 		
-		global timelapse_running
-		
-		if self.path=="/snapshot/":
+		if self.path=="/":
+			f = open('views/view.html',"rb")
 			self.send_response(200)
 			self.end_headers()
-			conn = httplib.HTTPConnection(CAMERA_IP, CAMERA_PORT)
-			conn.request("GET", "/image/")
-			self.wfile.write(conn.getresponse().read())
-			conn.close()
-		
-		if self.path=="/timelapse/start/":
-			timelapse_running.value = True
-			self.send_response(200)
-			self.end_headers()
-			conn = httplib.HTTPConnection(CAMERA_IP, CAMERA_PORT)
-			conn.request("POST", "/timelapse/start/")
-			conn.getresponse()
-			conn.close()
-			p = Process(target=interval_of_image_pulls,args=(timelapse_running,)).start()
-			
-		if self.path=="/timelapse/stop/":
-			timelapse_running.value = False
-			self.send_response(200)
-			self.end_headers()
-		
-		if self.path=="/temperature/":
-			self.send_response(200)
-			self.end_headers()
-			conn = httplib.HTTPConnection(CAMERA_IP, CAMERA_PORT)
-			conn.request("GET", "/temperature/")
-			print conn.getresponse().read()
-			conn.close()
-			return
+			self.wfile.write(f.read())
+			f.close()
 
 if __name__ == '__main__':
 	server = HTTPServer(('', HTTP_PORT), myHandler)
